7b73a77db82f42c17aac05d4b3be66ac006b280b
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2023-12-29 10:13:41 +0600

    update condition

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import re
 # search words
-search_words =["Java","Python","C++","go","javascript","sql","nodemon","ReAct"]
+search_words = ["Java", "Python", "c++", "go",
+                "javascript", "sql", "nodemon", "ReAct", "c#", "php"]
 
 # read txt file
-with open("./file.txt","r") as file:
+with open("./file.txt", "r") as file:
     data = file.read()
 
-    # match words
-    for word in search_words:
-        matches = re.findall(rf"\b{word.lower()}\b",data,flags=re.IGNORECASE)
-        print(f"{word} : {len(matches)}")
+# file data as list
+data = data.split("\n")
+
+# remove empty string from list
+data = list(filter(None, data))
+
+# convert list items as lower case
+data = [item.lower() for item in data]
+
+
+# match full word with case insensitive anc count
+for word in search_words:
+    match = data.count(word.lower())
+    print(f"{word} : {match}")
